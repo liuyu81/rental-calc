428b1201887af378a5d3fa6fa5559546c63f487b
LIU Yu <liuyu@opencps.net>
2013-11-04 21:53:06 +0800

    merged django view into calc.py

--- calc.py
+++ calc.py
@@ -64,6 +64,7 @@ def b2r(b, v=220.00, y=15, d=3):
 
 
 def main(args):
+    """console interface"""
     if len(args) < 2:
         print(("synopsis: %s r2b rate(r) [first_year_rental(v) "
                "[years(y) [years_per_cycle(d)]]]" % __file__))
@@ -90,5 +91,48 @@ def main(args):
     return os.EX_OK
 
 
+def django_main(request):
+    """wsgi/django interface"""
+    from django.http import HttpResponse
+    from django.template import loader, Context
+
+    data = request.REQUEST
+    op = data[u"op"] if u"op" in data else u'r2b'
+    v = float(data[u"v"].encode()) if u"v" in data else 220
+    y = int(data[u"y"].encode()) if u"y" in data else 15
+    d = int(data[u"d"].encode()) if u"d" in data else 3
+    r = float(data[u"r"].encode()) / 100.0 if u"r" in data else 0.05
+    b = float(data[u"b"].encode()) if u"b" in data else 4000.00
+    err = u""
+
+    if op == u'b2r':
+        try:
+            r = b2r(b, v, y, d)
+        except ValueError as e:
+            err = u"bad budget"
+            r = 0.0
+    if op == u'r2b':
+        try:
+            b = r2b(r, v, y, d)
+        except ValueError as e:
+            err = u"bad rate"
+            b = 0.0
+
+    response = HttpResponse(content_type='text/html')
+    tmpl = loader.get_template('calc.tmpl')
+    cntx = Context({
+        'v': v,
+        'y': y,
+        'd': d,
+        'b': b,
+        'r': r * 100.0,
+        'r2b': u"checked=checked" if op == u"r2b" else u"",
+        'b2r': u"checked=checked" if op == u"b2r" else u"",
+        'err': err
+    })
+    response.write(tmpl.render(cntx))
+    return response
+
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
+
